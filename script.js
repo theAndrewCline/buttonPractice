1f1c5f9104ca533d2bd7ee8ae270b027b62c47b1
Andrew Cline <acline@precisionplanting.com>
2024-11-14 13:54:55 -0600

    feat: add purple, pink, and orange
    per jack jack's request

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-var greenBtn = document.querySelector(".btn-green");
-var blueBtn = document.querySelector(".btn-blue");
-var yellowBtn = document.querySelector(".btn-yellow");
-var redBtn = document.querySelector(".btn-red");
-var background = document.querySelector(".background");
-var currentColor = "";
-var stuff = document.querySelector(".stuff");
+/**
+ * HELPERS
+ **/
 
-setTimeout(function() {
-  greenBtn.classList.add("animated","pulse")
-}, 250)
+/**
+ * @param {string} clickColor
+ */
+const checkColor = (clickColor) =>
+  rgbToHex(background.style.background).toLowerCase() ===
+  clickColor.toLowerCase();
 
-setTimeout(function() {
-  blueBtn.classList.add("animated","pulse")
-}, 500)
+/**
+ * @param {string} rgb
+ */
+function rgbToHex(rgb) {
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r, g, b)" into [r, g, b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
 
-setTimeout(function() {
-  yellowBtn.classList.add("animated","pulse")
-}, 750)
-setTimeout(function() {
-  redBtn.classList.add("animated","pulse")
-}, 1000)
+  let r = (+rgb[0]).toString(16),
+    g = (+rgb[1]).toString(16),
+    b = (+rgb[2]).toString(16);
 
-greenBtn.onclick = function() {
-  if (checkColor("rgb(34, 153, 84)")) {}
-  else {
-    background.style.background = "#229954"
-  }
-  setCurrentColor()
-}
-blueBtn.onclick = function() {
-  if(checkColor("rgb(41, 128, 185)")) {}
-  else {
-    background.style.background = "#2980B9"
-  }
-  setCurrentColor()
-}
-yellowBtn.onclick = function() {
-  if(checkColor("rgb(241, 196, 15)")) {}
-  else {
-    background.style.background = "#F1C40F"
-  }
-  setCurrentColor()
-}
-redBtn.onclick = function() {
-  if(checkColor("rgb(192, 57, 43)")){}
-  else {
-  background.style.background = "#C0392B"
-  }
-  setCurrentColor()
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  return "#" + r + g + b;
 }
 
-function checkColor(clickColor) {
-  console.log(currentColor, clickColor)
-  if (currentColor === clickColor) {
-    background.style.background = "#FFF"
-    return true
-  }
-  else {
-    return false
-  }
-}
+/**
+ * MAIN
+ **/
 
-function setCurrentColor() {
-  currentColor = background.style.background;
-}
-stuff.onclick = function() {
-  background.style.background = "#FFF"
-}
+const createButton = (colorName, hex) => {
+  const btn = document.createElement("button");
+
+  btn.id = colorName.toLowerCase();
+  btn.innerText = colorName;
+  btn.style.background = hex;
+  btn.onclick = switchColor(hex);
+  btn.classList = "btn";
+
+  return btn;
+};
+
+const btnContainer = document.querySelector(".btn-cont");
+const background = document.querySelector(".background");
+
+const stuff = document.querySelector(".stuff");
+
+const switchColor = (/** @type {string} */ hex) => () => {
+  if (checkColor(hex)) {
+    background.style.background = "#FFFFFF";
+  } else {
+    background.style.background = hex;
+  }
+};
+
+const buttons = [
+  createButton("Green", "#229954"),
+  createButton("Blue", "#2980B9"),
+  createButton("Yellow", "#F1C40F"),
+  createButton("Red", "#C0392B"),
+  createButton("Orange", "#ee9525"),
+  createButton("Purple", "#a525ee"),
+  createButton("Pink", "#ee25c4"),
+];
+
+buttons.forEach((btn) => {
+  btnContainer.appendChild(btn);
+});
+
+stuff.onclick = function () {
+  background.style.background = "#FFFFFF";
+};
+
+buttons.forEach((btn, i) => {
+  setTimeout(() => {
+    btn.classList.add("animated", "pulse");
+  }, 250 * i);
+});
